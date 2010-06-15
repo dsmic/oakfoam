88136074f4b1afe355d567cbfe3d3fac39c39cb9
Francois van Niekerk <flash.slash@gmail.com>
2010-06-16 00:54:54 +0200

    improve trials script

--- trials/run-trials.sh
+++ trials/run-trials.sh
@@ -5,8 +5,11 @@ BLACK="../oakfoam"
 #WHITE="/data/go/software/brown-1.0/brown"
 WHITE="/data/go/software/amigogtp-1.6/amigogtp/amigogtp"
 REFEREE="fuego"
-TWOGTP="gogui-twogtp -black \"$BLACK\" -white \"$WHITE\" -referee \"$REFEREE\" -games 10 -size 9 -alternate -sgffile trials"
-gogui -size 9 -program "$TWOGTP" -computer-both -auto
+GAMES=10
+COMMAND="gogui-twogtp-black param playouts_per_move 300"
+
+TWOGTP="gogui-twogtp -black \"$BLACK\" -white \"$WHITE\" -referee \"$REFEREE\" -games $GAMES -size 9 -alternate -sgffile trials"
+gogui -size 9 -program "$TWOGTP" -computer-both -auto -command "$COMMAND"
 
 gogui-twogtp -analyze trials.dat
 
