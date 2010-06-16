0c2d9f3fe81a9f84512e7c96404043bfd3f0763d
Francois van Niekerk <flash.slash@gmail.com>
2010-06-17 00:15:37 +0200

    add cmd line args

--- src/main.cc
+++ src/main.cc
@@ -2,13 +2,46 @@
   #error Error! "config.h" required
 #endif
 #include <config.h>
+#include <iostream>
 
 #include "Oakfoam.h"
 
-int main(int argc, char**argv)
+void printusage()
 {
-    Oakfoam oakfoam;
-    oakfoam.run();
-    
-    return 0;
+  std::cout << "Usage: oakfoam [OPTIONS]\n";
+  std::cout << "\n";
+  std::cout << "Options:\n";
+  std::cout << "  -h, --help        display usage and exit\n";
+  std::cout << "  -V, --version     display version and exit\n";
+  std::cout << "\n";
+  std::cout << "Report bugs to: " << PACKAGE_BUGREPORT << "\n";
+}
+
+int main(int argc, char* argv[])
+{
+  Oakfoam oakfoam;
+  
+  for(int i=1;i<argc;i++) 
+  {
+    std::string arg = argv[i];
+    if (arg=="-h" || arg=="--help" )
+    {
+      printusage();
+      return 0;
+    }
+    else if (arg=="-V" || arg=="--version" )
+    {
+      std::cout << PACKAGE_STRING << "\n";
+      return 0;
+    }
+    else
+    {
+      std::cout << "Invalid argument: " << arg << "\n";
+      return 1;
+    }
+  }
+  
+  oakfoam.run();
+  
+  return 0;
 }
