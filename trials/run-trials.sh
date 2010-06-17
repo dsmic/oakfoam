f5dada382c41ad45badbc76c9cb934e78ea9f0ef
Francois van Niekerk <flash.slash@gmail.com>
2010-06-17 04:36:52 +0200

    add time to script

--- trials/run-trials.sh
+++ trials/run-trials.sh
@@ -8,10 +8,11 @@ WHITE="/data/go/software/amigogtp-1.6/amigogtp/amigogtp"
 
 REFEREE="fuego"
 GAMES=10
+TIME="5m"
 COMMAND="gogui-twogtp-black param playouts_per_move 300"
 
 TWOGTP="gogui-twogtp -black \"$BLACK\" -white \"$WHITE\" -referee \"$REFEREE\" -games $GAMES -size 9 -alternate -sgffile trials"
-gogui -size 9 -program "$TWOGTP" -computer-both -auto -command "$COMMAND"
+gogui -size 9 -program "$TWOGTP" -computer-both -auto -time $TIME -command "$COMMAND"
 
 gogui-twogtp -analyze trials.dat
 
