651856134797159b63b551a6a732409e4b07c087
Francois van Niekerk <flash.slash@gmail.com>
2010-06-28 04:00:52 +0200

    update trials script

--- trials/run-trials.sh
+++ trials/run-trials.sh
@@ -6,12 +6,14 @@ BLACK="../oakfoam -c oakfoam.rc"
 WHITE="/data/go/software/amigogtp-1.6/amigogtp/amigogtp"
 #WHITE="fuego"
 
-REFEREE="fuego"
-GAMES=10
+#REFEREE="fuego"
+REFEREE="gnugo --mode gtp --chinese-rules --capture-all-dead"
+
+GAMES=100
 TIME="5m"
 
-TWOGTP="gogui-twogtp -black \"$BLACK\" -white \"$WHITE\" -referee \"$REFEREE\" -games $GAMES -size 9 -alternate -sgffile trials"
-gogui -size 9 -program "$TWOGTP" -computer-both -auto -time $TIME
+TWOGTP="gogui-twogtp -black \"$BLACK\" -white \"$WHITE\" -referee \"$REFEREE\" -games $GAMES -size 9 -komi 6.5 -alternate -sgffile trials"
+gogui -size 9 -komi 6.5 -program "$TWOGTP" -computer-both -auto -time $TIME
 
 gogui-twogtp -analyze trials.dat
 
