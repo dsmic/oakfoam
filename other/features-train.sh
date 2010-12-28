4ff2189940557bd1178867f204e50a3402896eed
Francois van Niekerk <flash.slash@gmail.com>
2010-12-28 02:19:27 +0200

    slightly improve training script

--- other/features-train.sh
+++ other/features-train.sh
@@ -2,6 +2,7 @@
 
 TEMPIDS="ids_`date +%F_%T`.tmp"
 TEMPLOG="log_`date +%F_%T`.tmp"
+TEMPMM="mm_`date +%F_%T`.tmp"
 OAKFOAM="../oakfoam"
 OAKFOAMLOG="../oakfoam --log $TEMPLOG"
 PROGRAM="gogui-adapter \"$OAKFOAMLOG\""
@@ -24,6 +25,15 @@ FEATUREIDCOUNT=`cat $TEMPIDS | wc -l`
 
 SEDFORMATMM=`cat $TEMPIDS | sed "s/\([0-9]*\) \(.*\)/s\/\2\/\1\/;/" | tr -d '\n'`
 
+MMFEATURES=`cat $TEMPIDS | sed "s/[0-9]* \([a-zA-Z0-9]*\):.*/\1/" | uniq -c | sed "s/^ *//"`
+
+MMHEADER="! ${FEATUREIDCOUNT}
+`echo "$MMFEATURES" | wc -l`
+${MMFEATURES}
+!"
+
+echo "$MMHEADER" > $TEMPMM
+
 echo "extracting competitions..." >&2
 i=0
 cat | while read GAME
@@ -31,28 +41,19 @@ do
   let "i=$i+1"
   echo -e "$i \t: '$GAME'" >&2
   echo -e "loadfeaturegammas ${INITIALPATTERNGAMMAS}\nparam features_output_competitions 1\nparam features_output_competitions_winnerfirst 1\nloadsgf \"$GAME\"" | gogui-adapter "$OAKFOAMLOG" > /dev/null
+  MMDATA=`cat $TEMPLOG | grep "^\[features\]:" | sed "s/\[features\]://" | sed "$SEDFORMATMM" | sed "s/^#.*/#/;s/[a-zA-Z0-9]*[*:] //"`
+  echo "$MMDATA" >> $TEMPMM
+  rm -f $TEMPLOG
 done
 
-echo "reformatting competitions..." >&2
-MMDATA=`cat $TEMPLOG | grep "^\[features\]:" | sed "s/\[features\]://" | sed "$SEDFORMATMM" | sed "s/^#.*/#/;s/[a-zA-Z0-9]*[*:] //"`
-
-MMFEATURES=`cat $TEMPIDS | sed "s/[0-9]* \([a-zA-Z0-9]*\):.*/\1/" | uniq -c | sed "s/^ *//"`
-
-MMHEADER="! ${FEATUREIDCOUNT}
-`echo "$MMFEATURES" | wc -l`
-${MMFEATURES}
-!
-"
-
-MMINPUT="${MMHEADER}${MMDATA}"
-
 echo "training..." >&2
-MMOUTPUT=`echo "$MMINPUT" | $MM`
+MMOUTPUT=`cat $TEMPMM | $MM`
 
 echo "done." >&2
 
 echo "$MMOUTPUT" | join $TEMPIDS - | sed "s/ */ /;s/^ //;s/^[0-9]* //"
 
+rm -f $TEMPMM
 rm -f $TEMPIDS
 rm -f $TEMPLOG
 
