0ef37019096cd1a8fd78e3760a6887fc12d7e355
Francois van Niekerk <flash.slash@gmail.com>
2011-01-07 03:24:29 +0200

    fix bug in features script

--- scripts/features/harvest-collection.sh
+++ scripts/features/harvest-collection.sh
@@ -3,16 +3,20 @@
 TEMPOUTPUT="collection_`date +%F_%T`.tmp"
 TEMPGAME="game_`date +%F_%T`.tmp"
 
+echo "" > $TEMPOUTPUT
+
 i=0
 cat | while read GAME
 do
   let "i=$i+1"
   echo -e "$i \t: '$GAME'" >&2
   ./harvest-patterns.sh "$GAME" > $TEMPGAME
-  ./harvest-combine.sh $TEMPOUTPUT $TEMPGAME > $TEMPOUTPUT
+  
+  DATA=`cat ${TEMPOUTPUT}`"\n"`cat ${TEMPGAME}`
+  echo -e "$DATA" | ./harvest-combine.sh | sort -rn > ${TEMPOUTPUT}
 done
 
-cat $TEMPOUTPUT | sort -rn
+cat $TEMPOUTPUT
 
 rm -f $TEMPOUTPUT
 rm -f $TEMPGAME
--- scripts/features/harvest-combine.sh
+++ scripts/features/harvest-combine.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
 
-if (( $# != 2 )); then
-  echo "Exactly 2 parameters required!" >&2
-  exit 1
-fi
-awk '{ count[$2] += $1 } END { for(elem in count) print count[elem], elem }' $1 $2
+cat | grep " 0x" | awk '{ count[$2] += $1 } END { for(elem in count) print count[elem], elem }'
 
