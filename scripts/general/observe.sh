28d6ee9835521002900bd86132d2f1ce06e6c6f0
Francois van Niekerk <flash.slash@gmail.com>
2011-01-11 20:04:38 +0200

    improve robustness of observe script

--- scripts/general/observe.sh
+++ scripts/general/observe.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tail -n`cat oakfoam.log | wc -l` -f oakfoam.log | ./observe.sed | gogui-display > /dev/null
+tail -n`cat oakfoam.log | wc -l` -F oakfoam.log | ./observe.sed | gogui-display > /dev/null
