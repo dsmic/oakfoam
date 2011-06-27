e57f1c9f33defa36cfda1bce248b5f9363ab1ed5
Francois van Niekerk <flash.slash@gmail.com>
2011-06-28 01:42:02 +0200

    improve callgrind script to skip initial loading

--- callgrind.sh
+++ callgrind.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-valgrind --tool=callgrind --dump-instr=yes ./oakfoam --nobook -c profile.gtp
+valgrind --tool=callgrind --dump-instr=yes --instr-atstart=no ./oakfoam --nobook -c profile.gtp & (sleep 2.5; callgrind_control -i on)
+
