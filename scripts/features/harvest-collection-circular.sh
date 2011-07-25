749cd1675053b6ae4b5f9b0b09490d6318e96ad0
Francois van Niekerk <flash.slash@gmail.com>
2011-07-25 17:37:36 +0200

    restructure large pattern script to function better

--- scripts/features/harvest-collection-circular.sh
+++ scripts/features/harvest-collection-circular.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 TEMPOUTPUT="collection_circ_`date +%F_%T`.tmp"
-TEMPGAME="game_circ_`date +%F_%T`.tmp"
+TEMPCOMBINED="combined_circ_`date +%F_%T`.tmp"
 
 SIZE=$1
 
@@ -12,14 +12,12 @@ cat | while read GAME
 do
   let "i=$i+1"
   echo -e "$i \t: '$GAME'" >&2
-  ./harvest-circular.sh "$GAME" $SIZE > $TEMPGAME
-  
-  DATA=`cat ${TEMPOUTPUT}`"\n"`cat ${TEMPGAME}`
-  echo -e "$DATA" | ./harvest-combine.sh | sort -rn > ${TEMPOUTPUT}
+  ./harvest-circular.sh "$GAME" $SIZE >> ${TEMPOUTPUT}
 done
 
-cat $TEMPOUTPUT
+cat ${TEMPOUTPUT} | ./harvest-combine.sh | sort -rn > ${TEMPCOMBINED}
+cat $TEMPCOMBINED
 
 rm -f $TEMPOUTPUT
-rm -f $TEMPGAME
+rm -f $TEMPCOMBINED
 
