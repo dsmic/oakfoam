548875ad725b8def3d9c9183ab8e24d2c6170654
Francois van Niekerk <flash.slash@gmail.com>
2012-01-13 00:35:58 +0200

    add Worker docs

--- src/engine/Worker.h
+++ src/engine/Worker.h
@@ -13,30 +13,43 @@ class Parameters;
 //from "Random.h":
 class Random;
 
+/** Worker thread management. */
 namespace Worker
 {
   class Thread;
   
+  /** Worker-specific settings. */
   class Settings
   {
     public:
+      /** The relevant thread. */
       Worker::Thread *thread;
+      /** The random number generator for this thread. */
       Random *rand;
   };
   
+  /** Worker thread. */
   class Thread
   {
     public:
+      /** Create a worker thread. */
       Thread(int i, Engine *eng);
       ~Thread();
       
+      /** Get the worker ID. */
       int getID() const { return id; };
+      /** Get the worker settings. */
       Worker::Settings *getSettings() const { return settings; };
+      /** Set the random seed for this worker. */
       void setRandomSeed(unsigned long seed);
       
+      /** Start working. */
       void start();
+      /** Stop working. */
       void stop();
+      /** Return when done working. */
       void wait();
+      /** Determine if this worker is busy. */
       bool isRunning() const { return running; };
     
     private:
@@ -64,18 +77,26 @@ namespace Worker
       void run();
   };
   
+  /** Pool of workers. */
   class Pool
   {
     public:
+      /** Create a pool given some parameters. */
       Pool(Parameters *prms);
       ~Pool();
       
+      /** Get the number of workers in this pool. */
       int getSize() const { return size; };
+      /** Get the first worker. */
       Worker::Thread *getThreadZero() const { return threads.front(); };
       
+      /** Start all the workers in this pool. */
       void startAll();
+      /** Stop all the workers in this pool. */
       void stopAll();
+      /** Wait for all the workers in this pool to finish. */
       void waitAll();
+      /** Set the seed for the workers in this pool. */
       void setRandomSeeds(unsigned long seed);
     
     private:
