ddafd18f3da99f0ec024366338fe4b2c55923d20
Francois van Niekerk <flash.slash@gmail.com>
2012-01-13 20:37:42 +0200

    add large portion of Parameters docs

--- src/engine/Parameters.h
+++ src/engine/Parameters.h
@@ -10,39 +10,64 @@ class Engine;
 class Random;
 #include "../gtp/Gtp.h"
 
+/** Parameter managment.
+ * It is generally accepted that most of these should be set via GTP.
+ * A "clear_board" command should usually be issued after these changes, to clear the MCTS tree.
+ */
 class Parameters
 {
   public:
     Parameters();
     ~Parameters();
     
+    /** Initial random seed.
+     * Set this to zero to select a new default seed.
+     */
     unsigned long rand_seed;
     
+    /** Engine associated with these parameters. */
     Engine *engine;
+    /** Current board size in use. */
     int board_size;
     
+    /** Move policies that the engine can follow. */
     enum MovePolicy
     {
       MP_PLAYOUT,
       MP_ONEPLY,
       MP_UCT
     };
+    /** String representation of the current move policy. */
     std::string move_policy_string;
+    /** Current move policy. */
     Parameters::MovePolicy move_policy;
-    
+   
+    /** Whether to make use of the currently loaded opening book. */
     bool book_use;
     
+    /** Number of threads to use. */
     int thread_count;
+    /** Jobs that the threads can perform. */
     enum ThreadJob
     {
       TJ_GENMOVE,
       TJ_PONDER,
       TJ_DONPLTS
     };
+    /** Current thread job. */
     Parameters::ThreadJob thread_job;
     
+    /** Number of playouts per move to perform.
+     * Not used if time setting are in use.
+     */
     int playouts_per_move;
+    /** Minimum number of playouts per move.
+     * Used when time settings are in use.
+     */
     int playouts_per_move_min;
+    /** Maximum number of playouts per move.
+     * Used when time setting are in use.
+     */
     int playouts_per_move_max;
     
     bool playout_atari_enabled;
@@ -66,114 +91,300 @@ class Parameters
     bool playout_avoid_selfatari;
     int playout_avoid_selfatari_size;
     
+    /** UCB exploration constant. */
     float ucb_c;
+    /** Initial UCB bias.
+     * Used when no playouts have gone through a node.
+     */
     float ucb_init;
     
+    /** Number of moves it takes for RAVE to decay?
+     * Set to zero to disable RAVE.
+     */
     int rave_moves;
+    /** Number of wins added to the RAVE statistics initially. */
     int rave_init_wins;
     
+    /** After this many playouts through a node, it will be expanded. */
     int uct_expand_after;
+    /** Whether to keep the sub-tree when a move is made.
+     * It does not make sense to enable pondering if this is not set.
+     */
     bool uct_keep_subtree;
+    /** Whether to make use of symmetry to reduce the initial branching factor. */
     bool uct_symmetry_use;
+    /** Whether to add a virtual loss on descent and remove it after updating the tree.
+     * This greatly improves performance in a multi-core setup.
+     */
     bool uct_virtual_loss;
+    /** Whether to use a lock-free concurrency strategy. */
     bool uct_lock_free;
+    /** Number of prior wins to add to atari moves in the tree. */
     int uct_atari_prior;
+    /** Number of prior wins to add to moves in the tree that match a pattern. */
     int uct_pattern_prior;
+    /** Whether to make use of progressive widening.
+     * When enabled, all node children are initially soft-pruned and the slowly unpruned, or widened, as playout pass through the node.
+     * @todo Include relevant formula.
+     */
     bool uct_progressive_widening_enabled;
+    /** Constant for progressive widening.
+     * @see uct_progressive_widening_enabled
+     */
     float uct_progressive_widening_a;
+    /** Constant for progressive widening.
+     * @see uct_progressive_widening_enabled
+     */
     float uct_progressive_widening_b;
+    /** Constant for progressive widening.
+     * @see uct_progressive_widening_enabled
+     */
     float uct_progressive_widening_c;
+    /** Parameter for progressive widening.
+     * @see uct_progressive_widening_enabled
+     */
     bool uct_progressive_widening_count_wins;
+    /** Constant for a bonus given to playouts, depending on the score difference.
+     * Set to zero to disable.
+     */
     float uct_points_bonus;
+    /** Constant for a bonus given to playouts, depending on the game length.
+     * Set to zero to disable.
+     */
     float uct_length_bonus;
+    /** Whether to make use of progressive bias.
+     * When enabled, a bias is added to nodes' urgencies.
+     * This bias decays as playouts pass through the node.
+     * @todo Include relevant formula.
+     */
     bool uct_progressive_bias_enabled;
+    /** Constant for progressive bias.
+     * @see uct_progressive_bias_enabled
+     */
     float uct_progressive_bias_h;
+    /** Constant for progressive bias.
+     * @see uct_progressive_bias_enabled
+     */
     bool uct_progressive_bias_scaled;
+    /** Constant for progressive bias.
+     * @see uct_progressive_bias_enabled
+     */
     bool uct_progressive_bias_relative;
+    /** Constant for adding a bias to urgency based on criticality.
+     * Set to zero to disable.
+     * @todo Include relevant formula.
+     */
     float uct_criticality_urgency_factor;
+    /** Parameter for criticality urgency.
+     * @see uct_criticality_urgency_factor
+     */
     bool uct_criticality_urgency_decay;
+    /** Constant for adjusting progressive widening based on criticality.
+     * Set to zero to disable.
+     * @todo Include relevant formula.
+     */
     float uct_criticality_unprune_factor;
+    /** Parameter for criticality unpruning.
+     * @see uct_criticality_unprune_factor
+     */
     bool uct_criticality_unprune_multiply;
+    /** Constant for criticality unpruning.
+     * @see uct_criticality_unprune_factor
+     */
     int uct_criticality_min_playouts;
+    /** Whether criticality updates should also affect siblings of the path to the root. */
     bool uct_criticality_siblings;
+    /** Whether terminal nodes should be propogated up the tree. */
     bool uct_terminal_handling;
+    /** Constant for adjusting progressive widening based on RAVE.
+     * Set to zero to disable.
+     * @todo Include relevant formula.
+     */
     float uct_rave_unprune_factor;
+    /** Parameter for RAVE unpruning.
+     * @see uct_rave_unprune_factor
+     */
     bool uct_rave_unprune_multiply;
     
+    /** Constant for decaying tree statistics.
+     * Set to one to disable.
+     * @todo Include relevant formula.
+     */
     float uct_decay_alpha;
+    /** Constant for decaying tree statistics.
+     * Set to one to disable.
+     * @todo Include relevant formula.
+     */
     float uct_decay_k;
+    /** Constant for decaying tree statistics.
+     * @see uct_decay_k
+     */
     float uct_decay_m;
     
+    /** Number of playouts between slow updates.
+     * Slow updates are things like checking if the best move can possible change.
+     */
     int uct_slow_update_interval;
+    /** When the last slow update occured. */
     int uct_slow_update_last;
     
+    /** Whether to stop early if the best move cannot change. */
     bool uct_stop_early;
+    /** Whether the last search stopped early. */
     bool early_stop_occured;
+    /** Most recent ration between the playouts of the best node and second best. */
     float uct_last_r2;
+    /** Number of playouts already in tree when current search started. */
     int uct_initial_playouts;
+    /** Whether we are in the cleanup phase of a game.
+     * This occurs on KGS after a scoring dispute.
+     */
     bool cleanup_in_progress;
     
+    /** Constant for decaying the territory map. */
     float territory_decayfactor;
+    /** Threshold for determining owners in the territory map. */
     float territory_threshold;
     
+    /** Whether to enable pondering.
+     * Pondering is thinking during the opponent's turn.
+     */
     bool pondering_enabled;
+    /** Maximum number of playouts that pondering can do.
+     * This value is compared with the number of playouts in the tree.
+     */
     int pondering_playouts_max;
     
+    /** Whether GTP commands can be interrupted.
+     * This occurs when "#gogui-interrupt" is sent when another command it working.
+     */
     bool interrupts_enabled;
     
+    /** Whether positional superko is in effect. */
     bool rules_positional_superko_enabled;
+    /** Whether superko should only be checked in the top tree ply. */
     bool rules_superko_top_ply;
+    /** Number of playouts after which superko should be checked. */
     int rules_superko_prune_after;
+    /** Whether to check superko when a playout passes through a node. */
     bool rules_superko_at_playout;
+    /** Whether all stones are treated as alive.
+     * Turning this off means that the territory map is used to determine alive and dead stones.
+     */
     bool rules_all_stones_alive;
     
+    /** Threshold at which to assume that we have a "sure win." */
     float surewin_threshold;
+    /** Whether a sure win is currently expected. */
     bool surewin_expected;
+    /** Bonus given to pass moves when a sure win is expected. */
     float surewin_pass_bonus;
+    /** Bonus given to moves that are adjacent to dead stones when a sure win is expected. */
     float surewin_touchdead_bonus;
+    /** Penalty given to moves that are in the opponent's area when a sure win is expected. */
     float surewin_oppoarea_penalty;
     
+    /** Minimum ratio before the game is resigned. */
     float resign_ratio_threshold;
+    /** Minimum portion of a game before before resignation is considered. */
     float resign_move_factor_threshold;
     
+    /** Time management constant.
+     * Time used ofr a move is remaining time divided by this constant.
+     */
     float time_k;
+    /** Amount of time to keep aside.
+     * This is used to mitigate lag problems that can occur in the last few moves of a game.
+     * Units are seconds.
+     */
     float time_buffer;
+    /** Minimum time per move.
+     * Units are seconds.
+     */
     float time_move_minimum;
+    /** Whether to ignore the time settings that are specified.
+     * If set, time settings will be ignored.
+     */
     bool time_ignore;
+    /** Maximum time to use per move.
+     * Units are seconds.
+     */
     float time_move_max;
     
+    /** Whether to output GoGui Live Gfx during thinking. */
     bool livegfx_on;
+    /** Number of playouts between each Live Gfx update. */
     int livegfx_update_playouts;
+    /** Time delay added after a Live Gfx update.
+     * Used to mitigate a GoGui issue.
+     */
     float livegfx_delay;
     
+    /** Number of children to output to SGF.
+     * Set to zero to output all.
+     */
     int outputsgf_maxchildren;
     
+    /** Whether to output debug info. */
     bool debug_on;
     
+    /** Whether to use only small features.
+     * Meant to aid incremental features for playouts.
+     */
     bool features_only_small;
+    /** Whether to output feature competitions after each move.
+     * Used for training feature gammas.
+     */
     bool features_output_competitions;
+    /** Whether to output feature competitions in a form for the MM tool.
+     * @see features_output_competitions
+     */
     bool features_output_competitions_mmstyle;
+    /** Whether to output a comparison of each move made with the features' ordering.
+     * Used to measure feature accuracy.
+     */
     bool features_ordered_comparison;
     
     #ifdef HAVE_MPI
+      /** Period between MPI syncs.
+       * Units are seconds.
+       */
       double mpi_update_period;
+      /** When the last MPI sync took place. */
       double mpi_last_update;
+      /** Maximum tree depth to sync with MPI. */
       int mpi_update_depth;
+      /** Threshold of playouts to sync with MPI.
+       * Only node with at least this percentage of the total playouts are synced.
+       */
       float mpi_update_threshold;
     #endif
     
+    /** The function that is called when a parameter is updated. */
     typedef void (*UpdateFunction)(void *instance, std::string id);
     
+    /** Add an int parameter. */
     void addParameter(std::string category, std::string id, int *ptr, int def, Parameters::UpdateFunction func=NULL, void *instance=NULL);
+    /** Add a float parameter. */
     void addParameter(std::string category, std::string id, float *ptr, float def, Parameters::UpdateFunction func=NULL, void *instance=NULL);
+    /** Add a double parameter. */
     void addParameter(std::string category, std::string id, double *ptr, double def, Parameters::UpdateFunction func=NULL, void *instance=NULL);
+    /** Add a boolean  parameter. */
     void addParameter(std::string category, std::string id, bool *ptr, bool def, Parameters::UpdateFunction func=NULL, void *instance=NULL);
+    /** Add a string parameter. */
     void addParameter(std::string category, std::string id, std::string *ptr, std::string def, Parameters::UpdateFunction func=NULL, void *instance=NULL);
+    /** Add a list parameter. */
     void addParameter(std::string category, std::string id, std::string *ptr, std::list<std::string> *options, std::string def, Parameters::UpdateFunction func=NULL, void *instance=NULL);
+    /** Add an unsigned long parameter. */
     void addParameter(std::string category, std::string id, unsigned long *ptr, unsigned long def, Parameters::UpdateFunction func=NULL, void *instance=NULL);
     
+    /** Set the given parameter to the given value.
+     * @return True if parameters was successfully set, otherwise false.
+     */
     bool setParameter(std::string id, std::string val);
+    /** Print parameters to the given Gtp engine for GoGui analyse commands. */
     void printParametersForGTP(Gtp::Engine *gtpe, std::string category="");
+    /** Print parameters to the given Gtp engine as part of a description. */
     void printParametersForDescription(Gtp::Engine *gtpe);
     
   private:
