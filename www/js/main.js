e6cb417d9abb514568eeb12e2c7787f4c7dff5ad
Francois van Niekerk <flash.slash@gmail.com>
2012-04-04 18:14:04 +0200

    Large web interface update. Switched to using Raphael JS library for gfx.

--- www/js/main.js
+++ www/js/main.js
@@ -6,6 +6,9 @@ var next_color;
 var engine_color='none';
 var passes;
 var game_over=false;
+var paper=null;
+var size_chg=true;
+var thinking=false;
 
 function xy2pos(x,y)
 {
@@ -15,6 +18,8 @@ function xy2pos(x,y)
 
 function doGtpCmdThink(cmd,args,func)
 {
+  thinking=true;
+  $('#pass, #genmove').button({disabled:true});
   $('#status').append('Thinking...<br/>\n');
   doGtpCmd(cmd,args,func)
 }
@@ -75,54 +80,91 @@ function isHoshi(x,y)
 
 function drawBoard(data)
 {
-  var board='';
+  var sz=board_size*24;
+  var lw=2;
+  var crad=11;
+  var bdr=4;
+  if (size_chg)
+  {
+    if (paper!=null)
+      paper.remove();
+    paper=Raphael('board',sz+2*bdr,sz+2*bdr);
+    size_chg=false;
+  }
+  paper.rect(1,1,sz-2+2*bdr,sz-2+2*bdr).attr({fill:'#c4a055', stroke:'#352b17', 'stroke-width':1});
+  for (i=0;i<board_size;i++)
+  {
+    paper.rect(i*24+12-lw/2+bdr,12-lw/2+bdr,lw,sz-24+lw).attr({fill:'#000000', stroke:'none'});
+    paper.rect(12-lw/2+bdr,i*24+12-lw/2+bdr,sz-24+lw,lw).attr({fill:'#000000', stroke:'none'});
+  }
+  for (x=0;x<board_size;x++)
+  {
+    for (y=0;y<board_size;y++)
+    {
+      if (isHoshi(x,y))
+        paper.circle(x*24+12+bdr,y*24+12+bdr,4).attr({fill:'#000000', stroke:'none'});
+    }
+  }
   for (x=0;x<board_size;x++)
   {
     for (y=0;y<board_size;y++)
     {
       pos=xy2pos(x,y)
       c=data[pos];
-      if (c=='B')
+
+      if (c=='B' || c=='W')
       {
+        if (c=='B')
+          paper.circle(x*24+12+bdr,y*24+12+bdr,crad).attr({fill:'#000000'});
+        else if (c=='W')
+          paper.circle(x*24+12+bdr,y*24+12+bdr,crad).attr({fill:'#ffffff',stroke:'#333333'});
+
         if ((c+":"+pos)==last_move)
-          board+='<img alt="'+pos+'" src="images/board/black-red.png"/>';
-        else
-          board+='<img alt="'+pos+'" src="images/board/black.png"/>';
+          paper.circle(x*24+12+bdr,y*24+12+bdr,4).attr({fill:'#7f7f7f',stroke:'none'});
       }
-      else if (c=='W')
-      {
-        if ((c+":"+pos)==last_move)
-          board+='<img alt="'+pos+'" src="images/board/white-red.png"/>';
-        else
-          board+='<img alt="'+pos+'" src="images/board/white.png"/>';
-      }
-      else if (x==0 && y==0)
-        board+='<img alt="'+pos+'" src="images/board/top-left.png"/>';
-      else if (x==0 && y==(board_size-1))
-        board+='<img alt="'+pos+'" src="images/board/top-right.png"/>';
-      else if (x==(board_size-1) && y==0)
-        board+='<img alt="'+pos+'" src="images/board/bottom-left.png"/>';
-      else if (x==(board_size-1) && y==(board_size-1))
-        board+='<img alt="'+pos+'" src="images/board/bottom-right.png"/>';
-      else if (x==0)
-        board+='<img alt="'+pos+'" src="images/board/top.png"/>';
-      else if (y==0)
-        board+='<img alt="'+pos+'" src="images/board/left.png"/>';
-      else if (x==(board_size-1))
-        board+='<img alt="'+pos+'" src="images/board/bottom.png"/>';
-      else if (y==(board_size-1))
-        board+='<img alt="'+pos+'" src="images/board/right.png"/>';
       else
       {
-        if (isHoshi(x,y))
-          board+='<img alt="'+pos+'" src="images/board/hoshi.png"/>';
-        else
-          board+='<img alt="'+pos+'" src="images/board/center.png"/>';
+        itt=paper.rect(x*24+bdr,y*24+bdr,24,24).attr({fill:'#000',opacity:0});
+        itt.data('pos',pos);
+        itt.data('hvx',x*24+bdr+12);
+        itt.data('hvy',y*24+bdr+12);
+        itt.data('hvo',null);
+
+        engine_to_play=engine_color=='both';
+        if (next_color=='B' && engine_color=='black')
+          engine_to_play=true;
+        else if (next_color=='W' && engine_color=='white')
+          engine_to_play=true;
+
+        if (!game_over && !engine_to_play)
+        {
+          itt.click(function()
+          {
+            doGtpCmd('play',next_color+'&'+this.data('pos'),refreshBoard);
+          });
+          itt.mouseover(function()
+          {
+            x=this.data('hvx');
+            y=this.data('hvy');
+            hvo=paper.circle(x,y,crad).attr({opacity:0.5});
+            if (next_color=='B')
+              hvo.attr({fill:'#000000'});
+            else
+              hvo.attr({fill:'#ffffff'});
+            this.data('hvo',hvo);
+            this.toFront();
+          });
+          itt.mouseout(function()
+          {
+            o=this.data('hvo');
+            if (o!=null)
+              o.remove();
+          });
+        }
       }
     }
-    board+="<br/>\n";
   }
-  $('#board').html(board);
+
   if (!game_over)
   {
     if (next_color=='B' && (engine_color=='black' || engine_color=='both'))
@@ -131,14 +173,11 @@ function drawBoard(data)
       doGtpCmdThink('genmove',next_color,refreshBoard);
     else
     {
-      $('#board img').click(function()
-      {
-        pos=$(this).attr('alt');
-        doGtpCmd('play',next_color+'&'+pos,refreshBoard);
-      });
+      thinking=false;
+      $('#pass, #genmove').button({disabled:false});
     }
   }
-  $('#page').width(board_size*24+200); // dynamic size
+  $('#page').width(board_size*24+200+2*bdr); // dynamic size
 }
 
 function updateStatus()
@@ -176,22 +215,25 @@ function refreshBoard()
 {
   $.getJSON('board_info.jsoncmd',function(data)
   {
+    if (data['size']!=board_size)
+      size_chg=true;
     board_size=data['size'];
     komi=data['komi'];
     moves=data['moves'];
     last_move=data['last_move'];
     next_color=data['next_color'];
     passes=data['passes'];
+
     if (passes>=2 || last_move=='RESIGN')
-    {
       game_over=true;
-      $('#pass, #genmove').button({disabled:true});
-    }
     else
-    {
       game_over=false;
+
+    if (thinking || game_over)
+      $('#pass, #genmove').button({disabled:true});
+    else
       $('#pass, #genmove').button({disabled:false});
-    }
+
     updateStatus();
     $.getJSON('board_pos.jsoncmd',function(data){drawBoard(data);});
   });
