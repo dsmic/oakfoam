208ef6464a21e256c7a904247fe3bba0ec1be263
Francois van Niekerk <flash.slash@gmail.com>
2013-01-24 14:50:42 +0200

    Parse more of the decision tree syntax

--- src/engine/DecisionTree.cc
+++ src/engine/DecisionTree.cc
@@ -3,19 +3,28 @@
 #include <cstdio>
 #include <fstream>
 #include <algorithm>
+#include <boost/lexical_cast.hpp>
 
 #define TEXT "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789<>=!."
 #define WHITESPACE " \t\r\n"
 
-DecisionTree::DecisionTree()
+DecisionTree::DecisionTree(std::vector<std::string> *a, DecisionTree::Node *r)
 {
+  attrs = a;
+  root = r;
+}
+
+DecisionTree::~DecisionTree()
+{
+  delete attrs;
+  delete root;
 }
 
 DecisionTree *DecisionTree::parseString(std::string rawdata)
 {
   std::string data = DecisionTree::stripWhitespace(rawdata);
   std::transform(data.begin(),data.end(),data.begin(),::toupper);
-  fprintf(stderr,"parsing: '%s'\n",data.c_str());
+  fprintf(stderr,"[DT] parsing: '%s'\n",data.c_str());
 
   unsigned int pos = data.find("(DT");
   if (pos == std::string::npos)
@@ -32,46 +41,66 @@ DecisionTree *DecisionTree::parseString(std::string rawdata)
   }
   pos++;
 
-  std::list<std::string> *attrs = DecisionTree::parseAttrs(data,pos);
+  std::vector<std::string> *attrs = DecisionTree::parseAttrs(data,pos);
   if (attrs == NULL)
     return NULL;
 
-  //TODO: parse NODE
+  if (attrs->size()!=2 || attrs->at(0)!="SPARSE" || attrs->at(1)!="NOCOMP") //TODO: extend to more tree types
+  {
+    fprintf(stderr,"[DT] Error! Invalid attributes for tree\n");
+    delete attrs;
+    return NULL;
+  }
+
+  DecisionTree::Node *root = DecisionTree::parseNode(data,pos);
+  if (root == NULL)
+  {
+    delete attrs;
+    return NULL;
+  }
 
   if (data[pos] != ')')
   {
     fprintf(stderr,"[DT] Error! Expected ')' at '%s'\n",data.substr(pos).c_str());
+    delete root;
+    delete attrs;
     return NULL;
   }
+  pos++;
 
-  //TODO: create new object
-  return NULL;
+  return new DecisionTree(attrs,root);
 }
 
-std::list<std::string> *DecisionTree::parseAttrs(std::string data, unsigned int &pos)
+std::vector<std::string> *DecisionTree::parseAttrs(std::string data, unsigned int &pos)
 {
   if (data[pos] == ']')
   {
     pos++;
-    return new std::list<std::string>();
+    return new std::vector<std::string>();
   }
 
   std::string attr = "";
-  while (DecisionTree::isText(data[pos]))
+  while (pos<data.length() && DecisionTree::isText(data[pos]))
   {
     attr += data[pos];
     pos++;
   }
-  fprintf(stderr,"[DT] attr:'%s'\n",attr.c_str());
+  //fprintf(stderr,"[DT] attr:'%s'\n",attr.c_str());
 
   if (data[pos] == '|' || data[pos] == ']')
   {
     if (data[pos] == '|')
       pos++;
-    std::list<std::string> *attrs = DecisionTree::parseAttrs(data,pos);
-    if (attrs == NULL)
+    std::vector<std::string> *attrstail = DecisionTree::parseAttrs(data,pos);
+    if (attrstail == NULL)
       return NULL;
-    attrs->push_front(attr);
+    std::vector<std::string> *attrs = new std::vector<std::string>();
+    attrs->push_back(attr);
+    for (unsigned int i=0;i<attrstail->size();i++)
+    {
+      attrs->push_back(attrstail->at(i));
+    }
+    delete attrstail;
     return attrs;
   }
   else
@@ -79,6 +108,88 @@ std::list<std::string> *DecisionTree::parseAttrs(std::string data, unsigned int 
     fprintf(stderr,"[DT] Error! Unexpected '%c' at '%s'\n",data[pos],data.substr(pos).c_str());
     return NULL;
   }
+}
+
+DecisionTree::Node *DecisionTree::parseNode(std::string data, unsigned int &pos)
+{
+  DecisionTree::Stats *stats = DecisionTree::parseStats(data,pos);
+  if (stats == NULL)
+    return NULL;
+
+  if (data.substr(pos,8) == "(WEIGHT[")
+  {
+    pos += 8;
+    float *num = DecisionTree::parseNumber(data,pos);
+    if (num == NULL)
+    {
+      delete stats;
+      return NULL;
+    }
+
+    if (data.substr(pos,2) != "])")
+    {
+      fprintf(stderr,"[DT] Error! Expected '])' at '%s'\n",data.substr(pos).c_str());
+      delete stats;
+      delete num;
+      return NULL;
+    }
+    pos += 2;
+
+    DecisionTree::Node *node = new DecisionTree::Node(stats,*num);
+    //fprintf(stderr,"[DT] !!!\n");
+
+    delete num;
+    return node;
+  }
+  else
+  {
+    //TODO: parse QUERY
+    return NULL;
+  }
+}
+
+DecisionTree::Stats *DecisionTree::parseStats(std::string data, unsigned int &pos)
+{
+  if (data.substr(pos,7) != "(STATS:")
+  {
+    fprintf(stderr,"[DT] Error! Expected '(STATS:' at '%s'\n",data.substr(pos).c_str());
+    return NULL;
+  }
+  pos += 7;
+
+  //TODO: parse STATPERMS
+
+  if (data[pos] != ')')
+  {
+    fprintf(stderr,"[DT] Error! Expected ')' at '%s'\n",data.substr(pos).c_str());
+    return NULL;
+  }
+  pos++;
+
+  return new DecisionTree::Stats();
+}
+
+float *DecisionTree::parseNumber(std::string data, unsigned int &pos)
+{
+  std::string s = "";
+  while (pos<data.length() && (data[pos]=='.' || (data[pos]>='0' && data[pos]<='9')))
+  {
+    s += data[pos];
+    pos++;
+  }
+
+  float n;
+  try
+  {
+    n = boost::lexical_cast<float>(s);
+  }
+  catch (boost::bad_lexical_cast &)
+  {
+    return NULL;
+  }
+  float *p = new float();
+  *p = n;
+  return p;
 }
 
 DecisionTree *DecisionTree::loadFile(std::string filename)
@@ -116,5 +227,16 @@ std::string DecisionTree::stripWhitespace(std::string in)
       out += in[i];
   }
   return out;
+}
+
+DecisionTree::Node::Node(Stats *s, float w)
+{
+  stats = s;
+  weight = w;
+}
+
+DecisionTree::Node::~Node()
+{
+  delete stats;
 }
 
--- src/engine/DecisionTree.h
+++ src/engine/DecisionTree.h
@@ -2,21 +2,46 @@
 #define DEF_OAKFOAM_DECISIONTREE_H
 
 #include <string>
-#include <list>
+#include <vector>
 
 /** Decision Tree for Feature Ensemble Method. */
 class DecisionTree
 {
   public:
-    /** Create a new decision tree. */
-    DecisionTree();
+    ~DecisionTree();
 
     static DecisionTree *parseString(std::string rawdata);
     static DecisionTree *loadFile(std::string filename);
   
   private:
+    class Stats
+    {
+    };
+
+    class Node
+    {
+      public:
+        Node(Stats *s, float w);
+        ~Node();
+
+        Stats *getStats() { return stats; };
+        float getWeight() { return weight; };
+
+      private:
+        Stats *stats;
+        float weight;
+    };
+
+    DecisionTree(std::vector<std::string> *a, DecisionTree::Node *r);
+
+    std::vector<std::string> *attrs;
+    Node *root;
+
     static std::string stripWhitespace(std::string in);
-    static std::list<std::string> *parseAttrs(std::string data, unsigned int &pos);
+    static std::vector<std::string> *parseAttrs(std::string data, unsigned int &pos);
+    static Node *parseNode(std::string data, unsigned int &pos);
+    static Stats *parseStats(std::string data, unsigned int &pos);
+    static float *parseNumber(std::string data, unsigned int &pos);
     static bool isText(char c);
 };
 
