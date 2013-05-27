1fe6bd04f46476c7795b2023887c20aa2fc64919
Francois van Niekerk <flash.slash@gmail.com>
2013-05-27 19:38:54 +0200

    Update plots for new move prediction data

--- scripts/move-prediction/plot.py
+++ scripts/move-prediction/plot.py
@@ -14,11 +14,231 @@ from matplotlib import cm
 
 jd = json.loads("".join(sys.stdin.readlines()))
 
-plot3d = False
-if 'type' in jd.keys():
-  plot3d = jd['type']=='3d'
+plottype = jd['type']
 
-if plot3d:
+if plottype == 'move-prediction': # 2d move prediction
+  fig = plt.figure()
+  fig.canvas.set_window_title(jd['title']) 
+  plt.title(jd['title'])
+  plt.xlabel('Move Rank')
+  plt.ylabel('Cumulative Probability')
+  plt.grid(True)
+  plt.ylim(0,1)
+  xmin = 1
+  xmax = 50
+  if 'xmax' in jd.keys():
+    xmax = int(jd['xmax'])
+  plt.xlim(xmin,xmax)
+  plt.yticks(np.append(np.arange(0,1,0.05),1))
+  plt.xticks(np.append(np.arange(xmin,xmax),xmax))
+  errk = 0.0
+  if 'errk' in jd.keys():
+    errk = float(jd['errk'])
+
+  table = []
+  table.append(range(xmin,xmax+1))
+  tablelabels = []
+  tablelabels.append('Move Rank')
+
+  c = 0
+  for f in jd['data']:
+    with open(f['file'], 'rb') as csvfile:
+      x = []
+      y = []
+      z = []
+      reader = csv.reader(csvfile)
+      for row in reader:
+        x.append(float(row[0]))
+        y.append(float(row[1]))
+        z.append(float(row[2]))
+
+      err = []
+      err1 = []
+      err2 = []
+      for i in range(len(x)):
+        if errk>0:
+          e = errk*math.sqrt(y[i]*(1-y[i])/z[i])
+        else:
+          e = 0
+        err.append(e)
+        err1.append(y[i]+e)
+        err2.append(y[i]-e)
+
+      lbl = f['label'] + ' (%.1f%%)' % (y[0]*100)
+      if len(jd['data']) == 1:
+        col = cm.spectral(0)
+      else:
+        col = cm.spectral(c*0.9/(len(jd['data'])-1),1)
+      c+=1
+      # p = plt.plot(x, z, label = lbl)
+      p = plt.plot(x, y, label = lbl, color = col)
+      # col = p[0].get_color()
+      if errk>0:
+        plt.fill_between(x, err1, err2, alpha = 0.2, color = col)
+        # plt.errorbar(x, z, yerr = err, fmt='.', color = col)
+
+      td = []
+      xi = 0
+      for i in table[0]:
+        while i > x[xi]:
+          xi += 1
+        td.append(y[xi])
+      table.append(td)
+      tablelabels.append(lbl)
+
+  plt.legend(loc=4)
+
+  sys.stdout.write('# ' + jd['title'] + '\n')
+
+  tt = len(tablelabels)
+  for i in range(tt):
+    sys.stdout.write('# ' + ('     | ')*i + ' '*5 + '+' + ('-'*7)*(tt-i-1) + '- %d: ' % i + tablelabels[i] +'\n')
+  sys.stdout.write('#' + (' |----|')*tt + '\n')
+
+  for i in range(len(table[0])):
+    sys.stdout.write(' ')
+    for j in range(len(table)):
+      if j == 0:
+        sys.stdout.write(' %6d' % table[j][i])
+      else:
+        sys.stdout.write(' %6.3f' % table[j][i])
+    sys.stdout.write('\n')
+
+elif plottype == 'stages-mp': # stages of move prediction
+  fig = plt.figure()
+  fig.canvas.set_window_title(jd['title']) 
+  plt.title(jd['title'])
+  plt.xlabel('Game Stage Center')
+  plt.ylabel('Move Prediction Accuracy')
+  plt.grid(True)
+  # plt.ylim(0,1)
+  xmin = 0
+  xmax = 300
+  stage = 30
+  if 'xmax' in jd.keys():
+    xmax = int(jd['xmax'])
+  if 'stage' in jd.keys():
+    stage = int(jd['stage'])
+  plt.xlim(xmin,xmax)
+  # plt.yticks(np.append(np.arange(0,1,0.02),1))
+  plt.xticks(np.append(np.arange(xmin,xmax,stage),xmax))
+  errk = 0.0
+  if 'errk' in jd.keys():
+    errk = float(jd['errk'])
+
+  c = 0
+  for f in jd['data']:
+    with open(f['file'], 'rb') as csvfile:
+      x = []
+      y = []
+      z = []
+      reader = csv.reader(csvfile)
+      for row in reader:
+        r0 = float(row[0])
+        r1 = float(row[1])
+        r2 = float(row[2])
+        r3 = float(row[3])
+        r4 = float(row[4])
+        r5 = float(row[5])
+        x.append((r0+r1)/2)
+        y.append(r2)
+        z.append(r4)
+
+      err = []
+      err1 = []
+      err2 = []
+      for i in range(len(x)):
+        if errk>0:
+          e = errk*math.sqrt(y[i]*(1-y[i])/z[i])
+        else:
+          e = 0
+        err.append(e)
+        err1.append(y[i]+e)
+        err2.append(y[i]-e)
+
+      lbl = f['label']
+      if len(jd['data']) == 1:
+        col = cm.spectral(0)
+      else:
+        col = cm.spectral(c*0.9/(len(jd['data'])-1),1)
+      c+=1
+      # p = plt.plot(x, z, label = lbl)
+      p = plt.plot(x, y, label = lbl, color = col)
+      # col = p[0].get_color()
+      if errk>0:
+        plt.fill_between(x, err1, err2, alpha = 0.2, color = col)
+        # plt.errorbar(x, z, yerr = err, fmt='.', color = col)
+
+  plt.legend(loc=4)
+
+elif plottype == 'stages-le': # stages of mean log-evidence
+  fig = plt.figure()
+  fig.canvas.set_window_title(jd['title']) 
+  plt.title(jd['title'])
+  plt.xlabel('Game Stage Center')
+  plt.ylabel('Mean Log-evidence')
+  plt.grid(True)
+  # plt.ylim(0,1)
+  xmin = 0
+  xmax = 300
+  stage = 30
+  if 'xmax' in jd.keys():
+    xmax = int(jd['xmax'])
+  if 'stage' in jd.keys():
+    stage = int(jd['stage'])
+  plt.xlim(xmin,xmax)
+  # plt.yticks(np.append(np.arange(0,1,0.02),1))
+  plt.xticks(np.append(np.arange(xmin,xmax,stage),xmax))
+  errk = 0.0
+  if 'errk' in jd.keys():
+    errk = float(jd['errk'])
+
+  c = 0
+  for f in jd['data']:
+    with open(f['file'], 'rb') as csvfile:
+      x = []
+      y = []
+      z = []
+      reader = csv.reader(csvfile)
+      for row in reader:
+        r0 = float(row[0])
+        r1 = float(row[1])
+        r2 = float(row[2])
+        r3 = float(row[3])
+        r4 = float(row[4])
+        r5 = float(row[5])
+        x.append((r0+r1)/2)
+        y.append(r3)
+        z.append(r5)
+
+      err = []
+      err1 = []
+      err2 = []
+      for i in range(len(x)):
+        if errk>0:
+          e = errk*math.sqrt(z[i])
+        else:
+          e = 0
+        err.append(e)
+        err1.append(y[i]+e)
+        err2.append(y[i]-e)
+
+      lbl = f['label']
+      if len(jd['data']) == 1:
+        col = cm.spectral(0)
+      else:
+        col = cm.spectral(c*0.9/(len(jd['data'])-1),1)
+      c+=1
+      # p = plt.plot(x, z, label = lbl)
+      p = plt.plot(x, y, label = lbl, color = col)
+      # col = p[0].get_color()
+      if errk>0:
+        plt.fill_between(x, err1, err2, alpha = 0.2, color = col)
+        # plt.errorbar(x, z, yerr = err, fmt='.', color = col)
+
+  plt.legend(loc=4)
+
+elif plottype == '3d': # legacy 3d mp plot
   fig = plt.figure()
 
   ax = fig.add_subplot(111, projection='3d')
@@ -76,7 +296,7 @@ if plot3d:
   ax.set_zlim(0,1)
   ax.set_ylim(0,xmax)
 
-else: # 2d plot
+else: # legacy 2d mp plot
   fig = plt.figure()
   fig.canvas.set_window_title(jd['title']) 
   plt.title(jd['title'])
